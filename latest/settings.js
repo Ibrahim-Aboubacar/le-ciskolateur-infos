7f980109fe462e41e7e268994e67571fda97d5d1
Ibrahim Aboubacar Cissé <103621495+Ibrahim-Aboubacar@users.noreply.github.com>
2023-11-28 11:01:56 +0000

    Update settings.js

--- latest/settings.js
+++ latest/settings.js
@@ -1,4 +1,4 @@
 {
-  "rate": {"value": 189, "updated_at": "27/11/2023 à 15:23"},
+  "rate": {"value": 189, "updated_at": "28/11/2023 à 11:01"},
   "app_update": {"current_version": "1.1.0", "updated_at": "07/10/2023", "desc": "/1.1.0/description.md"}
 }
