8ea7c2de8d59e4ee02220599abf169287a7ec1ba
Ibrahim Aboubacar Cissé <103621495+Ibrahim-Aboubacar@users.noreply.github.com>
2023-12-02 06:33:42 +0000

    Update settings.js

--- latest/settings.js
+++ latest/settings.js
@@ -3,7 +3,7 @@
     "value": 192,
       "updated_at": "01/12/2023 à 12:40:00",
       "rateCountStart": 140,
-      "userActivityServer" : "https://ciskolateur.000webhostapp.com/user-activity.php"
+      "userActivityServer" : "https://cisk000olateur.000webhostapp.com/user-activity.php"
   },
   "app_update": {
     "current_version": "1.1.0",
