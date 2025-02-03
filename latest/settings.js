4f60c45d9ecba4799e844cae0521f7f113cf6f6b
Ibrahim Aboubacar Cissé <103621495+Ibrahim-Aboubacar@users.noreply.github.com>
2025-02-03 20:45:18 +0000

    Update settings.js

--- latest/settings.js
+++ latest/settings.js
@@ -7,7 +7,7 @@
       "agentNumber": "+233544621369"
   },
   "app_update": {
-    "current_version": "1.2.1",
+    "current_version": "1.1.1",
       "updated_at": "19/07/2024", 
       "desc": "/1.1.1/description.md"
   }
