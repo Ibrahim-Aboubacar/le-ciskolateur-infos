e09060d3449a30b5abdda88fb4949c7f817b3a2a
Ibrahim Aboubacar Cissé <103621495+Ibrahim-Aboubacar@users.noreply.github.com>
2025-02-24 12:53:38 +0000

    Update settings.js 248

--- latest/settings.js
+++ latest/settings.js
@@ -1,7 +1,7 @@
 {
   "rate": {
     "value": 248,
-      "updated_at": "22 Février 2025 À 06:00:00",
+      "updated_at": "24 Février 2025 À 13:00:00",
       "rateCountStart": 100,
       "userActivityServer" : "https://slim-app.test/user-activity.php",
       "agentNumber": "+233544621369"
