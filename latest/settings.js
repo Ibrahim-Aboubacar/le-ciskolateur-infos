c8d356395f33491e4173aa0c492dd54b43a16a29
Ibrahim Cissé <ibrahimaboubacarcisse@gmail.com>
2026-06-04 12:20:03 +0000

    CiskoBot: Update FCFA rate to 219 (04 juin 2026 @ 12:20:03)

--- latest/settings.js
+++ latest/settings.js
@@ -1,1 +1,1 @@
-{"rate": {"value": 220,"updated_at": "03 juin 2026 @ 12:10:04","rateCountStart": 154,"userActivityServer" : "https://slim-app.test/user-activity.php","agentNumber": "+233544621369"},"app_update": {"current_version": "1.1.1","updated_at": "19/07/2024", "desc": "/1.1.1/description.md"}}
+{"rate": {"value": 219,"updated_at": "04 juin 2026 @ 12:20:03","rateCountStart": 139,"userActivityServer" : "https://slim-app.test/user-activity.php","agentNumber": "+233544621369"},"app_update": {"current_version": "1.1.1","updated_at": "19/07/2024", "desc": "/1.1.1/description.md"}}
