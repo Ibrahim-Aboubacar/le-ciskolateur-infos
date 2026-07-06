4f299d6595490aaaa42b1511e66b0cdf8fabbf2e
Ibrahim Aboubacar Ibrahim <103621495+Ibrahim-Aboubacar@users.noreply.github.com>
2026-07-06 21:40:46 +0000

    Update settings.js 203

--- latest/settings.js
+++ latest/settings.js
@@ -1,1 +1,1 @@
-{"rate": {"value": 200,"updated_at": "05 juillet 2026 @ 17:50:04","rateCountStart": 146,"userActivityServer" : "https://slim-app.test/user-activity.php","agentNumber": "+233544621369"},"app_update": {"current_version": "1.1.1","updated_at": "19/07/2024", "desc": "/1.1.1/description.md"}}
+{"rate": {"value": 203,"updated_at": "06 juillet 2026 @ 11:40:12","rateCountStart": 146,"userActivityServer" : "https://slim-app.test/user-activity.php","agentNumber": "+233544621369"},"app_update": {"current_version": "1.1.1","updated_at": "19/07/2024", "desc": "/1.1.1/description.md"}}
